8f3c139e720da502861b90d6b497aeb2002fd3a1
sjtroxel <sjtroxel@protonmail.com>
2026-06-05 12:20:12 -0500

    complete Lesson 11 (scope)

---
+++ Lesson11/scope.py
@@ -0,0 +1,18 @@
+name = "sjtroxel"
+count = 1
+
+def another():
+    color = "blue"
+    global count
+    count += 1
+    print(count)
+
+    def greeting(name):
+        nonlocal color
+        color = "red"
+        print(color)
+        print(name)
+
+    greeting("sjtroxel")
+
+another()
