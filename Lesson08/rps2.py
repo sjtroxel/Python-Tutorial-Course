4013df37951e7123541260ad455193b4f19770a7
sjtroxel <sjtroxel@protonmail.com>
2026-06-04 19:11:27 -0500

    finish Lessons 08-09 (loops & functions)

---
+++ Lesson08/rps2.py
@@ -0,0 +1,49 @@
+import sys
+import random
+from enum import Enum
+
+class RPS(Enum):
+    ROCK = 1
+    PAPER = 2
+    SCISSORS = 3
+
+playagain = True
+
+while playagain:
+    
+    playerchoice = input(
+        "\nEnter...\n1 for Rock,\n2 for Paper, or\n3 for Scissors:\n\n")
+
+    player = int(playerchoice)
+
+    if player < 1 or player > 3:
+        sys.exit("You must enter 1, 2, or 3.")
+
+    computerchoice = random.choice("123")
+
+    computer = int(computerchoice)
+
+    print("\nYou chose " + playerchoice + ".")
+    print("Python chose " + computerchoice + ".\n")
+
+    if player == 1 and computer == 3:
+        print("🎉 You win!")
+    elif player == 2 and computer == 1:
+        print("🎉 You win!")
+    elif player == 3 and computer == 2:
+        print("🎉 You win!")
+    elif player == computer:
+        print("😮 Tie game!")
+    else:
+        print("🐍 Python wins!")
+
+    playagain = input("\nPlay again? \nY for Yes or \nQ to Quit \n\n")
+
+    if playagain.lower() == "y":
+        continue
+    else:
+        print("\n🎉🎉🎉🎉")
+        print("Thank you for playing!\n")
+        playagain = False
+
+sys.exit("Bye! 👋")
